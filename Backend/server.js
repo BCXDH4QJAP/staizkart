71d90c1e616535ff1ea5eebe705e5078844d5fcf
RITIK RAJ <155656337+BCXDH4QJAP@users.noreply.github.com>
2025-12-25 14:07:34 +0530

    Update server.js

--- Backend/server.js
+++ Backend/server.js
@@ -1,1 +1,45 @@
+const express = require("express");
+const bodyParser = require ("body-parser");
+const cors = require("cors");
 
+const app = express();
+const PORT = 3000;
+
+app.use(cors());
+app.use(bodyParser.json());
+
+//fake database for learning
+let users = [];
+
+//sign up
+app.post("/signup", (req, res) => {
+    const { username, password } = req.body;
+
+    //check if user already exists
+    const userExists = users.find(u => u.username === username);    
+    if (userExists) {
+        return res.json({success: false, message:"User already exists"});
+    }
+
+    users.push({username, password});
+    res.json({success: true, message:"Signup successful"});
+});
+
+//sign in 
+app.post("/signin", (req, res) => {
+    const {username, password} = req.body;
+
+    const user = users.find(
+    u => u.username === username && u.password === password
+  );
+
+  if (user) {
+    res.json({ success: true, message: "Login successful" });
+  } else {
+    res.json({ success: false, message: "Invalid credentials" });
+  }
+});
+
+app.listen(PORT, () => {
+  console.log("Server running on http://localhost:3000");
+});
